1f0c35fe91926c3e2b1ee1449aae5a7759ce8286
Jairo Mariscal <jairomariscal@Jairos-MacBook-Air.local>
2023-01-11 18:42:31 -0800

    lets go

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+function generatePassword(){
+  var confirmPasswordLength = window.prompt("How many characters would you like your password to have?")
+}
